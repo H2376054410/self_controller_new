9edb08646d36f6863f2baedef94c2b264db7f4a9
LHL <2376054410@qq.com>
2024-06-07 22:09:15 +0800

    ss

--- self_controller/bsp/stm32/RTT+HAL/applications/drv_HAL_Main.h
+++
@@ -1,6 +0,0 @@
-#ifndef __DRV_HAL_MAIN_H__
-#define __DRV_HAL_MAIN_H__
-
-void Error_Handler(void);
-
-#endif
--- self_controller/bsp/stm32/RTT+HAL/applications/func_TxHandle.h
+++
@@ -1,8 +0,0 @@
-#ifndef __FUNC_TXHANDLE_H__
-#define __FUNC_TXHANDLE_H__
-
-#include "stm32f1xx_hal.h"
-
-extern void TxHandle_Init(void);
-
-#endif /* __FUNC_TXHANDLE_H__ */
